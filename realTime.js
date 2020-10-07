13338cbcfe0b70bead79b02af9e31225c7d1693a
김보군 <rlaqhrns93@naver.com>
2020-10-07 21:43:19 +0900

    save

--- realTime.js
+++ realTime.js
@@ -1,26 +1,21 @@
-const clockTitle = $(".realtime");
 
 function getTime(){
+    const clockContainer = document.querySelector(".chart");
+    
+    const clockTitle = clockContainer.querySelector(".realtime");
     const date = new Date();
     const minutes = date.getMinutes();
     const hours = date.getHours();
     const years = date.getFullYear();
     const months = date.getMonth()+1;
     const days = date.getDate();
+    const time =`${years}.${months}.${days} ${
+        hours < 10 ? `0${hours}`: hours
+    }:${
+        minutes < 10 ? `0${minutes}` : minutes
+    } 기준`; 
     
-    clockTitle.html(`${years}.${months}.${days} ${
-        hours < 10 ? `0${hours}`: hours
-    }:${
-        minutes < 10 ? `0${minutes}` : minutes
-    } 기준`);
-    console.log(`${years}.${months}.${days} ${
-        hours < 10 ? `0${hours}`: hours
-    }:${
-        minutes < 10 ? `0${minutes}` : minutes
-    } 기준`);
+    clockTitle.innerHTML = time;
+    console.log(time);
 
 }
-function init(){
-    setInterval(getTime, 1000);
-}
-init();
